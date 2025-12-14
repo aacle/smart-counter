b87f76a645704b790c4155b2e8f8888126bb5303
Abhishek <Abhishek.jorwaal@gmail.com>
2025-12-14 23:18:45 +0000

    Fix try 1

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,18 +19,6 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
-// Force compileSdkVersion for all Android library plugins (fixes package_info_plus and similar)
-subprojects {
-    afterEvaluate {
-        if (project.hasProperty("android")) {
-            val android = project.extensions.getByName("android")
-            if (android is com.android.build.gradle.LibraryExtension) {
-                android.compileSdk = 35
-            }
-        }
-    }
-}
-
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
